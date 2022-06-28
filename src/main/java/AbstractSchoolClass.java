7223974f207f1f58c583819a46ab8d1d533c7764
RamanBohdan <lo63iksport004@gmail.com>
2022-06-28 21:53:43 +0300

    add new project file

---
+++ src/main/java/AbstractSchoolClass.java
@@ -0,0 +1,13 @@
+public class AbstractSchoolClass {
+
+    String teacherName;
+    int classNumber;
+    String studentName;
+
+    public AbstractSchoolClass(String teacherName, int classNumber, String studentName) {
+        this.teacherName = teacherName;
+        this.classNumber = classNumber;
+        this.studentName = studentName;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<AbstractSchoolClass> schools = new ArrayList<>(128);
+        schools.add(new AbstractSchoolClass("Inna", 1, "Bob"));
+        schools.add(new AbstractSchoolClass("Kamilla", 2, "Michael"));
+        schools.add(new AbstractSchoolClass("Tamara",3,"Konstantin"));
+        schools.add(new AbstractSchoolClass("Elena",4,"Nataliya"));
+        schools.add(new AbstractSchoolClass("Vasilisa", 4, "Mariya"));
+        schools.add(new AbstractSchoolClass("Valeriya", 6, "Sofiya"));
+        schools.add(new AbstractSchoolClass("Juliya", 7,"Raman"));
+        schools.add(new AbstractSchoolClass("Vladislava",8, "Kirill"));
+        schools.add(new AbstractSchoolClass("Vera", 9,"Kira"));
+        schools.add(new AbstractSchoolClass("Svetlana",10,"Maksim"));
+        schools.add(new AbstractSchoolClass("Alina",11,"Aleksandr"));
+    }
+}
